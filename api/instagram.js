7277c87d9a6ec15121e5d01152dd90ecbe0c9963
conciencia0 <conciencia.ia0@gmail.com>
2025-08-07 16:08:52 -0300

    Update instagram.js

--- api/instagram.js
+++ api/instagram.js
@@ -2,7 +2,7 @@
 
 export default async function handler(req, res) {
   // El ID de tu usuario de Instagram
-  const userId = '17841476081965186'; 
+  const userId = '24654366867514458'; 
   
   // CORRECTO: Leemos la variable de entorno llamada 'INSTAGRAM_ACCESS_TOKEN'
   const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
